8fd7fe3543b58d194ae2a1cecec48f472bc4f47d
Panshulp <panshul_purusharthy27@milton.edu>
2025-11-12 09:04:37 -0500

    Added a constructor to factory

---
+++ src/Airplane.java
@@ -0,0 +1,20 @@
+public class Airplane {
+
+    // Instance variables
+    // Used throughout the class
+    // Placed at the top
+    // Have public in front
+    public int capacity;
+    public String name;
+    public String model;
+    public boolean isDoubleDecker;
+    public int wingspan;
+    public Airplane () {
+        capacity = 615;
+        name = "Etihad Airways";
+        wingspan = 80;
+        model = "Airbus-380";
+        isDoubleDecker = true;
+
+    }
+}
--- src/Factory.java
+++ src/Factory.java
@@ -1,5 +1,7 @@
+import java.sql.SQLOutput;
+
 public class Factory {
-    public int yearFounded = 1798;
+    public int yearFounded = 1799;
     public String factoryName = "Airplanes";
     public boolean isOpen = true;
 
@@ -12,6 +14,7 @@ public class Factory {
     //Constructors are special types of methods that have same name to the class
     //Constructors allow us to make a factory (in this case)
     //scope is the whole class
+    //no void
     public Factory(){
         factoryInfo();
         isOpen = false;
@@ -20,6 +23,20 @@ public class Factory {
 
         //todo: change the variable yearFounded to be a different value
         //todo: call factoryInfo();
+        // Type varName = value
+        // Object of type airplane
+        Airplane plane1 = new Airplane();
+
+        System.out.println(plane1.name);
+        System.out.println(plane1.capacity);
+        System.out.println(plane1.isDoubleDecker);
+        System.out.println(plane1.wingspan);
+        System.out.println(plane1.model);
+
+        Airplane plane2 = new Airplane();
+        //todo: make 2 more airplane objects
+        //todo: for one of the objects chnge all the instance variables
+        //todo: for both objects print all the instance variables
     }
 
     public void factoryInfo (){
